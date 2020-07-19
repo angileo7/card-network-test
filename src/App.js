97133e94fbfbb5f3e6726012aabaf23bec6e42ff
angileo7 <angel.bookmart@gmail.com>
2020-07-19 15:01:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Detector from "./Components/Detector/detector";
 function App() {
   return (
     <div className="App">
+    <h3>Type card number in order to get the network name for such card</h3>
       <Detector />
     </div>
   );
